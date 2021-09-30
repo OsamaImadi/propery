53d04ba532cc0db9d05791f3d60c9c9a55f740f7
OsamaImadi <osamaimadi@yahoo.com>
2021-09-30 18:17:09 +0500

    Hardcoded port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  let port = process.env.PORT || 3000
+  let port = 3000
   console.log("App listening on port: ", port)
   await app.listen(port);
 }
